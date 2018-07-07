eb96c2ef5e466e924774174da1aeefd36a72535f
samsales1409 <38017063+samsales1409@users.noreply.github.com>
2018-07-07 17:58:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//Temprature conversion with calaculation
+var temperatureInCelsius=40;
+var celsiusToFahrenheit;
+celsiusToFahrenheit=(temperatureInCelsius*9)/5+32;
+console.log(`Temprature of ${temperatureInCelsius} celcious will be ${celsiusToFahrenheit}  farennfight`);
+var temperatureInFahrenheit=104;
+var fahrenheitToCelsius;
+fahrenheitToCelsius=((temperatureInFahrenheit-32)* 5)/9;
+console.log(`Temprature of ${temperatureInFahrenheit} farennfight will be ${fahrenheitToCelsius} celcious`);
